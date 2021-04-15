b16374fe9bc3cc007041be5470dc6533edbed08a
Niccolò <6045018+Blackmane@users.noreply.github.com>
2021-04-15 21:57:18 +0000

    Committing clang-format changes

--- code/samples/cliClient.cpp
+++ code/samples/cliClient.cpp
@@ -30,13 +30,11 @@
 
 std::string askForInput(std::string meta);
 
-void printResponse(gemini::Response * response);
+void printResponse(gemini::Response *response);
 
 bool askToRedirect(std::string newUrl);
 
 void printFailure(std::string message, std::string meta);
-
-
 
 int main(int argc, char const *argv[]) {
 
@@ -51,38 +49,37 @@ int main(int argc, char const *argv[]) {
 
   try {
     do {
-        PrintGetFROM(hostname, port);
-        auto response = gemini::GeminiClient::request(hostname, port);
+      PrintGetFROM(hostname, port);
+      auto response = gemini::GeminiClient::request(hostname, port);
 
-        again = false;
-        switch (response->statusCodeFirst)
-        {
-        case gemini::response::INPUT :
-            // TODO: handle request with input
-            askForInput(response->meta);
-            again = true;
-            //hostname = response->meta;
-            break;
-        case gemini::response::SUCCESS :
-            printResponse(response.get());
-            break;
-        case gemini::response::REDIRECT :
-            again = askToRedirect(response->meta);
-            // TODO: check if is relative url
-            hostname = response->meta;
-            break;
-        case gemini::response::TEMPORARY_FAILURE :
-            printFailure("Temporary failure", response->meta);
-            break;
-        case gemini::response::PERMANENT_FAILURE :
-            printFailure("Permanent failure", response->meta);
-            break;
-        case gemini::response::CLIENT_CERTIFICATE_REQUIRED :
-            printFailure("Error with certificate", response->meta);
-            break;
-        default:
-            break;
-        }
+      again = false;
+      switch (response->statusCodeFirst) {
+      case gemini::response::INPUT:
+        // TODO: handle request with input
+        askForInput(response->meta);
+        again = true;
+        // hostname = response->meta;
+        break;
+      case gemini::response::SUCCESS:
+        printResponse(response.get());
+        break;
+      case gemini::response::REDIRECT:
+        again = askToRedirect(response->meta);
+        // TODO: check if is relative url
+        hostname = response->meta;
+        break;
+      case gemini::response::TEMPORARY_FAILURE:
+        printFailure("Temporary failure", response->meta);
+        break;
+      case gemini::response::PERMANENT_FAILURE:
+        printFailure("Permanent failure", response->meta);
+        break;
+      case gemini::response::CLIENT_CERTIFICATE_REQUIRED:
+        printFailure("Error with certificate", response->meta);
+        break;
+      default:
+        break;
+      }
     } while (again);
   } catch (std::exception &e) {
     std::cout << "Error: " << e.what() << std::endl;
@@ -91,46 +88,39 @@ int main(int argc, char const *argv[]) {
   return 0;
 }
 
-
 std::string askForInput(std::string meta) {
-    std::string input;
-    std::cout << "----- ----- ----- ----- ----- ----- -----" << std::endl;
-    std::cout << "Response: ask for input: " << meta
-              << std::endl;
-    std::cin >> input;
-    std::cout << "----- ----- ----- ----- ----- ----- -----" << std::endl;
-    return input;
+  std::string input;
+  std::cout << "----- ----- ----- ----- ----- ----- -----" << std::endl;
+  std::cout << "Response: ask for input: " << meta << std::endl;
+  std::cin >> input;
+  std::cout << "----- ----- ----- ----- ----- ----- -----" << std::endl;
+  return input;
 }
 
-
-void printResponse(gemini::Response * response) {
+void printResponse(gemini::Response *response) {
+  std::cout << "----- ----- ----- ----- ----- ----- -----" << std::endl;
+  std::cout << "Response: " << response->statusCodeFirst
+            << response->statusCodeSecond << " " << response->meta << std::endl;
+  std::cout << "----- ----- ----- ----- ----- ----- -----" << std::endl;
+  if (response->body != "") {
+    std::cout << response->body << std::endl;
     std::cout << "----- ----- ----- ----- ----- ----- -----" << std::endl;
-    std::cout << "Response: " << response->statusCodeFirst
-              << response->statusCodeSecond << " " << response->meta
-              << std::endl;
-    std::cout << "----- ----- ----- ----- ----- ----- -----" << std::endl;
-    if (response->body != "") {
-      std::cout << response->body << std::endl;
-      std::cout << "----- ----- ----- ----- ----- ----- -----" << std::endl;
-    }
+  }
 }
-
 
 bool askToRedirect(std::string newUrl) {
-    std::string input;
-    std::cout << "\nRedirect to " << newUrl << "? (y/N)" << std::endl;
-    std::cin >> input;
-    if (!(input[0] == 'y' || input[0] == 'Y')) {
-        return false;
-    }
-    std::cout << std::endl;
-    return true;
+  std::string input;
+  std::cout << "\nRedirect to " << newUrl << "? (y/N)" << std::endl;
+  std::cin >> input;
+  if (!(input[0] == 'y' || input[0] == 'Y')) {
+    return false;
+  }
+  std::cout << std::endl;
+  return true;
 }
 
 void printFailure(std::string message, std::string meta) {
-    std::cout << "----- ----- ----- ----- ----- ----- -----" << std::endl;
-    std::cout << "Failure: " << message << "\n"
-              << meta
-              << std::endl;
-    std::cout << "----- ----- ----- ----- ----- ----- -----" << std::endl;
+  std::cout << "----- ----- ----- ----- ----- ----- -----" << std::endl;
+  std::cout << "Failure: " << message << "\n" << meta << std::endl;
+  std::cout << "----- ----- ----- ----- ----- ----- -----" << std::endl;
 }
